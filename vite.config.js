82f38e327453913b3a7baaf55949eb60a2f16853
Jim Solomon <jsolomon@smxtech.com>
2026-04-23 21:16:37 -0400

    Missing config

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { viteStaticCopy } from 'vite-plugin-static-copy'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    viteStaticCopy({
-      targets: [{ src: 'assets', dest: '.' }]
-    }),
-  ],
-})
+    base: "/frostlog/",
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [{ src: "assets", dest: "." }],
+        }),
+    ],
+});
